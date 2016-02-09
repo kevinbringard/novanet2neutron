5c1cbd1e217b2273465f2fc1cc80f726f1273daf
Kevin Bringard <kevinbri@cisco.com>
2016-02-09 13:17:36 -0700

    Fix VLAN code and ZONE/PHYSNES checks

--- create-conf.py
+++ create-conf.py
@@ -5,22 +5,23 @@ from netaddr import *
 import MySQLdb
 import os
 import re
+import sys
 
 NOVA_CONF = "/etc/nova/nova.conf"
 NEUTRON_CONF = "/etc/neutron/plugins/ml2/ml2_conf.ini"
 ZONE = ""
 PHYSNET = ""
 
-if ZONE is None:
-  print "You need to set ZONE in $0"
+if ZONE is None or ZONE == "":
+  print "You need to set ZONE in this script"
   sys.exit(1)
-elif PHYSNET is None:
-  print "You need to set PHYSNET in $0"
+elif PHYSNET is None or PHYSNET == "":
+  print "You need to set PHYSNET in this script"
   sys.exit(1)
 
 if os.path.isfile(NOVA_CONF):
-	nova_config = ConfigParser.ConfigParser()
-	nova_config.read(NOVA_CONF)
+    nova_config = ConfigParser.ConfigParser()
+    nova_config.read(NOVA_CONF)
 
 if os.path.isfile(NEUTRON_CONF):
         neutron_config = ConfigParser.ConfigParser()
@@ -34,23 +35,23 @@ nova_db_string = nova_config.get('DEFAULT', 'sql_connection')
 neutron_db_string = neutron_config.get('database', 'connection')
 
 def get_nova_db_info(db_string):
-	# Get the DB Host and Name
-	split_string = re.split(r':', db_string)
-	split_string = re.split(r'@', split_string[-1])
-	split_string = re.split(r'/', split_string[-1])
-	db_host = split_string[0]
-	split_string = re.split(r'\?', split_string[-1])
-	db_name = split_string[0]
+    # Get the DB Host and Name
+    split_string = re.split(r':', db_string)
+    split_string = re.split(r'@', split_string[-1])
+    split_string = re.split(r'/', split_string[-1])
+    db_host = split_string[0]
+    split_string = re.split(r'\?', split_string[-1])
+    db_name = split_string[0]
 
-	# Start over to get the DB pass
-	split_string = re.split(r':', db_string)
-	db_pass = re.split(r'@', split_string[-1])
+    # Start over to get the DB pass
+    split_string = re.split(r':', db_string)
+    db_pass = re.split(r'@', split_string[-1])
 
-	# And finally, get the db_user
-	split_string = re.split(r':', db_string)
+    # And finally, get the db_user
+    split_string = re.split(r':', db_string)
         db_user = [ re.sub(r'//','', string) for string in split_string ]
 
-	return db_host, db_name, db_pass[0], db_user[1]
+    return db_host, db_name, db_pass[0], db_user[1]
 
 def get_neutron_db_info(db_string):
         # Get the DB Host and Name
@@ -72,22 +73,22 @@ def get_neutron_db_info(db_string):
         return db_host, db_name, db_pass[0], db_user[1]
 
 def get_all_networks():
-	cursor = MySQLdb.cursors.DictCursor(conn)
-	cursor.execute(
-	   "SELECT uuid from networks where project_id != 'NULL'")
-	uuids = cursor.fetchall()
-	return uuids
+    cursor = MySQLdb.cursors.DictCursor(conn)
+    cursor.execute(
+       "SELECT uuid from networks where project_id != 'NULL'")
+    uuids = cursor.fetchall()
+    return uuids
 
 def get_network_info(uuid):
-	cursor = MySQLdb.cursors.DictCursor(conn)
-	cursor.execute(
-	   "SELECT * from networks where uuid = '%s' and deleted = 0" % uuid)
-	network = cursor.fetchall()
-	return network
+    cursor = MySQLdb.cursors.DictCursor(conn)
+    cursor.execute(
+       "SELECT * from networks where uuid = '%s' and deleted = 0" % uuid)
+    network = cursor.fetchall()
+    return network
 
 def get_dhcp_end(cidr):
-	net = IPNetwork(cidr)
-	return net[-2]
+    net = IPNetwork(cidr)
+    return net[-2]
 
 # Pull the DB info out of the config files
 nova_db_host, nova_db_name, nova_db_pass, nova_db_user = get_nova_db_info(nova_db_string)
@@ -100,38 +101,38 @@ conn = MySQLdb.connect(
     db=nova_db_name)
 
 for network in get_all_networks():
-	uuid = network['uuid']
-	network_info = get_network_info(uuid)
-	# print network_info
-	name = network_info[0]['label']
-	cidr_v4 = network_info[0]['cidr']
-	gateway_v4 = network_info[0]['gateway']
-	dhcp_start = network_info[0]['dhcp_start']
-	dhcp_end = get_dhcp_end(cidr_v4)
-	dns_server1 = network_info[0]['dns1']
-	dns_server2 = network_info[0]['dns2']
-	tenant_id = network_info[0]['project_id']
-    vlan = network_info[0]['vlan']
-	dns_servers = ""
-	if dns_server1 and dns_server2:
-		dns_servers = "%s,%s" % (dns_server1, dns_server2)
-	elif dns_server1:
-		dns_servers = dns_server1
-	elif dns_server2:
-		dns_servers = dns_server2
+    uuid = network['uuid']
+    network_info = get_network_info(uuid)
+    # print network_info
+    name = network_info[0]['label']
+    cidr_v4 = network_info[0]['cidr']
+    gateway_v4 = network_info[0]['gateway']
+    dhcp_start = network_info[0]['dhcp_start']
+    dhcp_end = get_dhcp_end(cidr_v4)
+    dns_server1 = network_info[0]['dns1']
+    dns_server2 = network_info[0]['dns2']
+    tenant_id = network_info[0]['project_id']
+        vlan = network_info[0]['vlan']
+    dns_servers = ""
+    if dns_server1 and dns_server2:
+        dns_servers = "%s,%s" % (dns_server1, dns_server2)
+    elif dns_server1:
+        dns_servers = dns_server1
+    elif dns_server2:
+        dns_servers = dns_server2
 
-	section = "network_" + ZONE + ":" + name
-	novanet2neutron_config.add_section(section)
-	novanet2neutron_config.set(section, 'zone', ZONE)
-	novanet2neutron_config.set(section, 'name', name)
-	novanet2neutron_config.set(section, 'physnet', PHYSNET)
-	novanet2neutron_config.set(section, 'cidr_v4', cidr_v4)
-	novanet2neutron_config.set(section, 'gateway_v4', gateway_v4)
-	novanet2neutron_config.set(section, 'dhcp_start', dhcp_start)
-	novanet2neutron_config.set(section, 'dhcp_end', dhcp_end)
-	novanet2neutron_config.set(section, 'dns_servers', dns_servers)
-	novanet2neutron_config.set(section, 'tenant_id', tenant_id)
-	novanet2neutron_config.set(section, 'vlan', vlan)
+    section = "network_" + ZONE + ":" + name
+    novanet2neutron_config.add_section(section)
+    novanet2neutron_config.set(section, 'zone', ZONE)
+    novanet2neutron_config.set(section, 'name', name)
+    novanet2neutron_config.set(section, 'physnet', PHYSNET)
+    novanet2neutron_config.set(section, 'cidr_v4', cidr_v4)
+    novanet2neutron_config.set(section, 'gateway_v4', gateway_v4)
+    novanet2neutron_config.set(section, 'dhcp_start', dhcp_start)
+    novanet2neutron_config.set(section, 'dhcp_end', dhcp_end)
+    novanet2neutron_config.set(section, 'dns_servers', dns_servers)
+    novanet2neutron_config.set(section, 'tenant_id', tenant_id)
+    novanet2neutron_config.set(section, 'vlan', vlan)
 
 # Open our FH
 cfgfile = open("novanet2neutron.conf", 'w')
