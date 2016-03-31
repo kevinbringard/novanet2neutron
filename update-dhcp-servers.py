ef7d7be2b9b6d7f7a9823fb40e47f6000e22373b
Kevin Bringar <kevinbringard@gmail.com>
2016-03-31 15:30:48 +0000

    Add code to update DHCP servers to match dnsmasq.conf

---
+++ update-dhcp-servers.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import MySQLdb
+import ConfigParser
+
+from novanet2neutron import common
+
+def update_dhcp_ip(port_id, dhcp_server):
+    cursor = MySQLdb.cursors.DictCursor(neutron_conn)
+    cursor.execute("""
+       UPDATE ipallocations
+       SET ip_address = %s 
+       WHERE port_id = %s 
+    """, (dhcp_server, port_id))
+    cursor.connection.commit()
+
+def get_dhcp_port(network_id):
+    cursor = MySQLdb.cursors.DictCursor(neutron_conn)
+    cursor.execute(
+       "SELECT ipallocations.port_id FROM ml2_port_bindings, ipallocations WHERE ml2_port_bindings.host LIKE 'neutron-dhcp%%' AND ipallocations.port_id = ml2_port_bindings.port_id AND network_id = '%s'" % network_id)
+    dhcp_port = cursor.fetchall()
+
+    return dhcp_port
+
+def get_all_network_ids():
+    cursor = MySQLdb.cursors.DictCursor(neutron_conn)
+    cursor.execute(
+       "SELECT id FROM networks")
+    uuids = cursor.fetchall()
+
+    return uuids
+
+CONF = ConfigParser.ConfigParser()
+common.load_config(CONF, 'novanet2neutron.conf')
+
+neutron_conn = MySQLdb.connect(
+    host=CONF.get('neutron_db', 'host'),
+    user=CONF.get('neutron_db', 'user'),
+    passwd=CONF.get('neutron_db', 'password'),
+    db=CONF.get('neutron_db', 'name'))
+
+cursor = MySQLdb.cursors.DictCursor(neutron_conn)
+
+networks = get_all_network_ids()
+neutronc = common.get_neutron_client()
+mappings = {}
+for section in CONF.sections():
+    if not section.startswith('network_'):
+        continue
+    mappings[section] = {}
+
+    for option in CONF.options(section):
+        mappings[section][option] = CONF.get(section, option)
+
+
+    zone = CONF.get(section, 'zone')
+    network_name = CONF.get(section, 'name')
+    if zone == network_name:
+        name = zone
+    else:
+        name = "%s" % (network_name)
+
+    dhcp_server = CONF.get(section, 'dhcp_server')
+    network_id = common.get_network(neutronc, name)
+    # print mappings
+    print "network: %s" % network_id
+    print "dhcp_server: %s" % dhcp_server
+
+    dhcp_port = get_dhcp_port(network_id)
+    print "dhcp_port: %s" % dhcp_port[0]['port_id']
+    update_dhcp_ip(dhcp_port[0]['port_id'], dhcp_server)
